236e4dcdabbaf38fe8f63bf75b963a4951639dc1
Asim Kamal <asimkamalk@gmail.com>
2025-12-04 21:19:55 +0500

    refactor: Convert synchronous file operations to asynchronous
    - Replace readFileSync/writeFileSync with callback-based async versions
    - Implement nested callback structure for sequential file operations
    - Comment out old synchronous code for reference

--- 01-read-write-file/index.js
+++ 01-read-write-file/index.js
@@ -1,10 +1,25 @@
 const fs = require("fs");
 
-const textInp = fs.readFileSync("./txt/input.txt", "utf-8");
-console.log(textInp);
+// Blocking, synchronous way
+// const textInp = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textInp);
 
-const textOut = `Text from output.txt method--> writeFileSync ${textInp}. \n Created on ${Date.now()}`;
+// const textOut = `Text from output.txt method--> writeFileSync ${textInp}. \n Created on ${Date.now()}`;
 
-fs.writeFileSync("./txt/output.txt", textOut);
+// fs.writeFileSync("./txt/output.txt", textOut);
 
-console.log("File has been written");
+// console.log("File has been written");
+
+// Non-Blocking asynchronous way
+
+// Call Back Hell
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  if (err) return console.log("Err");
+  fs.readFile(`./txt/input.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+    fs.writeFile("./txt/append.txt", `${data1}\n ${data2}`, (err) => {
+      console.log("file hass been written");
+    });
+  });
+});
+console.log("Will read file");
