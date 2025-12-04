7c1690e633b91a88257bcfaed3f287de268683e4
Asim Kamal <asimkamalk@gmail.com>
2025-12-04 20:36:15 +0500

    feat: Implement file read and write operations

--- 01-read-write-file/index.js
+++ 01-read-write-file/index.js
@@ -1,3 +1,10 @@
 const fs = require("fs");
 
-fs.readFileSync("");
+const textInp = fs.readFileSync("./txt/input.txt", "utf-8");
+console.log(textInp);
+
+const textOut = `Text from output.txt method--> writeFileSync ${textInp}. \n Created on ${Date.now()}`;
+
+fs.writeFileSync("./txt/output.txt", textOut);
+
+console.log("File has been written");
