9c922e17fad1106254ddd8a91cc36c519ea4b68d
Asim Kamal <asimkamalk@gmail.com>
2025-12-06 03:14:42 +0500

    Created callback hell and used slugify

---
+++ 03-async-js/index.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const superagent = require("superagent");
+
+fs.readFile(`${__dirname}/dog.txt`, "utf-8", (err, data) => {
+  console.log("Breed:", data);
+  superagent
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .end((err, res) => {
+      if (err) return console.log(err);
+      console.log(res.body.message);
+      fs.writeFile("dog-image.txt", res.body.message, (err) => {
+        if (err) return console.log(err);
+        console.log("Random dog image saved");
+      });
+    });
+});
