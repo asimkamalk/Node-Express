ef5c1ebc4e7171acecebd2da575006690ad36aee
Asim Kamal <asimkamalk@gmail.com>
2025-12-05 20:03:37 +0500

    feat: Implement dynamic product page and refactor template logic

--- 02-node-routing-mini-project/index.js
+++ 02-node-routing-mini-project/index.js
@@ -2,21 +2,9 @@ const http = require("http");
 const url = require("url");
 const fs = require("fs");
 
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
+const slugify = require("slugify");
 
-  if (!product.organic) {
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  }
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  return output;
-};
+const replaceTemplate = require("./module/replaceTemplate");
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
@@ -37,10 +25,11 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  console.log(req.url);
+  const { query, pathname } = url.parse(req.url, true);
 
   // Overview Page
-  if (pathName === "/" || pathName === "/overview") {
+  if (pathname === "/" || pathname === "/overview") {
     const cardsHtml = dataObj
       .map((el) => replaceTemplate(tempCard, el))
       .join("");
@@ -53,11 +42,16 @@ const server = http.createServer((req, res) => {
     res.end(output);
 
     // Product Page
-  } else if (pathName === "/product") {
-    res.end("This is PRODUCT");
+  } else if (pathname === "/product") {
+    const product = dataObj[query.id];
+    res.writeHead(200, {
+      "content-type": "text/html",
+    });
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
 
     // API
-  } else if (pathName === "/api") {
+  } else if (pathname === "/api") {
     res.writeHead(200, {
       "content-type": "application/json",
     });
